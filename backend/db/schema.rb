68667bff97b7eef974f2e444dde3b815cc010aa2
Shane Zackery <szackery1244@gmail.com>
2019-06-01 13:27:47 -0700

    move over and run migrations

---
+++ backend/db/schema.rb
@@ -0,0 +1,90 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2018_07_17_044210) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "collection_resources", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "collection_id", null: false
+    t.bigint "resource_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["collection_id"], name: "index_collection_resources_on_collection_id"
+    t.index ["resource_id"], name: "index_collection_resources_on_resource_id"
+  end
+
+  create_table "collections", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "owner_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["owner_id"], name: "index_collections_on_owner_id"
+  end
+
+  create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "resource_id", null: false
+    t.bigint "fan_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["fan_id"], name: "index_favorites_on_fan_id"
+    t.index ["resource_id"], name: "index_favorites_on_resource_id"
+  end
+
+  create_table "resource_tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "resource_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["resource_id"], name: "index_resource_tags_on_resource_id"
+    t.index ["tag_id"], name: "index_resource_tags_on_tag_id"
+  end
+
+  create_table "resources", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "url", null: false
+    t.text "description", null: false
+    t.bigint "author_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "author"
+    t.string "type"
+    t.index ["author_id"], name: "index_resources_on_author_id"
+    t.index ["category_id"], name: "index_resources_on_category_id"
+  end
+
+  create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "message", null: false
+    t.integer "evidence_rating", null: false
+    t.integer "useful_rating", null: false
+    t.integer "detail_rating", null: false
+    t.integer "recommend_rating", null: false
+    t.bigint "author_id", null: false
+    t.bigint "resource_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_reviews_on_author_id"
+    t.index ["resource_id"], name: "index_reviews_on_resource_id"
+  end
+
+  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,3 +1,6 @@
+Category.destroy_all
+User.destroy_all
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +8,62 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = [
+  "Mental Health & Psychology",
+  "Climate Change",
+  "Self-Defense",
+  "Labor & Economics",
+  "Politics",
+  "History",
+  "Laws & The Constitution",
+  "Schools & Education",
+  "Legal Support",
+  "Fundraising",
+  "Technology",
+  "Medicine",
+  "Environmental Science & Issues",
+  "Gender",
+  "Sexuality",
+  "Organization Building",
+  "Transportation",
+  "Finance",
+  "Business",
+  "Family & Parenting",
+  "Housing",
+  "Personal Development",
+  "Spirituality",
+  "Religion",
+  "Scholarships",
+  "Miscellaneous",
+  "International Affairs",
+  "Trafficking",
+  "Criminal Justice",
+  "Prisons",
+  "War on Drugs",
+  "Race & Ethnicity",
+  "Global Poverty",
+  "Health Care",
+  "Child Labor",
+  "Economic Justice",
+  "U.S. Poverty",
+  "HIV/AIDS",
+  "Immigration",
+  "Conflicts in the Middle East",
+  "U.S. Military Activities & Spending",
+  "Debt",
+  "Water",
+  "Food Justice",
+  "Civil Rights & Discrimination",
+  "Government Spending",
+  "Humanitarian Crises",
+  "Slavery"
+  "Constructive Engagement of Conflict"
+]
+
+categories.each do |category|
+	Category.create(title: category)
+end
+
+user = User.create(username: "cosmosiris", email: ENV['ADMIN_EMAIL'], password: ENV['ADMIN_LOGIN_PASSWORD'], admin: true)
+
